a1fcbb3659021f9ce748c2be233ea11cc4a05df3
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-05 12:47:58 -0500

    Started working on 25.1

---
+++ Python Labs/Loops/25.1_Count input length without spaces, periods, exclamation points, or commas.py
@@ -0,0 +1,23 @@
+"""
+Given a line of text as input, output the number of characters excluding spaces, periods, exclamation points, or commas.
+
+Ex: If the input is:
+
+Listen, Mr. Jones, calm down.
+
+the output is:
+
+21
+
+"""
+
+user_text = str(input())
+count = 0
+
+for x in user_text:
+    if x == " " or x == "," or x == "." or x =="!":
+        count = 0
+    else:
+        count += 1
+
+print(count)
