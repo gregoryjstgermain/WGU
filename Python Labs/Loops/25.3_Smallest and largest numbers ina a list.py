eb50d46766191e6cb20386ed8e756a1b9c6d966b
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-06 15:44:34 -0500

    Started 25.3, got stuck Posted on stack overflow

---
+++ Python Labs/Loops/25.3_Smallest and largest numbers ina a list.py
@@ -0,0 +1,29 @@
+"""
+Write a program that reads a list of integers into a list as long as the integers are greater than zero,
+then outputs the smallest and largest integers in the list.
+
+Ex: If the input is:
+
+10
+5
+3
+21
+2
+-6
+
+the output is:
+
+2 and 21
+
+You can assume that the list of integers will have at least 2 values.
+
+"""
+
+user_input = int(input())
+input_list = []
+
+while 0 < user_input:
+    user_input = int(input())
+    input_list.append(user_input)
+
+print(f'{min(input_list)} and {max(input_list)}')
