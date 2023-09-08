7f709be826accbe9afca6a7869b8b7816f8b504f
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-08 16:45:25 -0500

    Created new package for functions Created and finished 26.1

---
+++ Python Labs/Functions/26.1_Track laps to miles.py
@@ -0,0 +1,35 @@
+"""
+One lap around a standard high-school running track is exactly 0.25 miles. Define a function named laps_to_miles that takes a number of laps as a parameter, and returns the number of miles. Then, write a main program that takes a number of laps as an input, calls function laps_to_miles() to calculate the number of miles, and outputs the number of miles.
+
+Output each floating-point value with two digits after the decimal point, which can be achieved as follows:
+print(f'{your_value:.2f}')
+
+Ex: If the input is:
+
+7.6
+
+the output is:
+
+1.90
+
+Ex: If the input is:
+
+2.2
+
+the output is:
+
+0.55
+
+The program must define and call the following function:
+def laps_to_miles(user_laps)
+
+"""
+def laps_to_miles(user_laps):
+    lap_mile = 0.25
+    user_miles = user_laps * lap_mile
+    print(f'{user_miles:.2f}')
+    return user_miles
+
+if __name__ == '__main__':
+    user_laps = float(input())
+    laps_to_miles(user_laps)
