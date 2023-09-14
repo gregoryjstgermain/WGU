7cd24caf069a131ba2da374e21f88adfbe98ea0e
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-14 15:50:00 -0500

    Created and started 28.2

---
+++ Python Labs/Lists and Dictionaries/28.2_Word frequencies.py
@@ -0,0 +1,26 @@
+"""
+Write a program that reads a list of words. Then, the program outputs those words and their frequencies (case insensitive).
+
+Ex: If the input is:
+
+hey Hi Mark hi mark
+
+the output is:
+
+hey 1
+Hi 2
+Mark 2
+hi 2
+mark 2
+
+Hint: Use lower() to set each word to lowercase before comparing.
+
+"""
+
+phrase = input()
+lower_phrase = phrase.lower()
+mod_phrase = phrase.split()
+mod_lower_phrase = lower_phrase.split()
+
+for word in mod_lower_phrase:
+    print(word, mod_lower_phrase.count(word))
