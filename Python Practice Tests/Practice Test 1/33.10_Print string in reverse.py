07c22559bdf42b9fe7656dfcd28cb316dd2a30be
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-18 11:06:32 -0500

    Created and started 33.10

---
+++ Python Practice Tests/Practice Test 1/33.10_Print string in reverse.py
@@ -0,0 +1,22 @@
+"""
+Write a program that takes in a line of text as input, and outputs that line of text in reverse.
+The program repeats, ending when the user enters "Done", "done", or "d" for the line of text.
+
+Ex: If the input is:
+
+Hello there
+Hey
+done
+
+then the output is:
+
+ereht olleH
+yeH
+
+
+"""
+phrase = str()
+
+while phrase != "d" and phrase != "done" and phrase != "Done":
+    phrase = input()
+    print(phrase [::-1])
