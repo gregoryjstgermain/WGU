1e96ed8441e21461c62848e89be0c458572dedb1
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-18 09:53:58 -0500

    Created and finished 33.5

---
+++ Python Practice Tests/Practice Test 1/33.5_Input and formatted output - right-facing arrow.py
@@ -0,0 +1,33 @@
+"""
+Given input characters for an arrowhead and arrow body, print a right-facing arrow.
+
+Ex: If the input is:
+
+*
+#
+
+Then the output is:
+
+      #
+******##
+******###
+******##
+      #
+
+
+"""
+
+
+base_char = input()
+head_char = input()
+
+row1 = '      ' + head_char
+row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
+row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
+''' Type your code here. '''
+
+print(row1)
+print(row2)
+print(row3)
+print(row2)
+print(row1)
