2062d896e2d78aed64ba8997af59f4252ac9dbf0
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-19 08:41:10 -0500

    Working on 33.15 33.10 is still WIP

--- Python Practice Tests/Practice Test 1/33.15_Parsing dates.py
+++ Python Practice Tests/Practice Test 1/33.15_Parsing dates.py
@@ -27,3 +27,45 @@ all_dates = []
 
 new_date = date.split('\n')
 print(new_date)
+
+
+def get_month_as_int(monthString):
+
+    if monthString == 'January':
+        month_int = 1
+    elif monthString == 'February':
+        month_int = 2
+    elif monthString == 'March':
+        month_int = 3
+    elif monthString == 'April':
+        month_int = 4
+    elif monthString == 'May':
+        month_int = 5
+    elif monthString == 'June':
+        month_int = 6
+    elif monthString == 'July':
+        month_int = 7
+    elif monthString == 'August':
+        month_int = 8
+    elif monthString == 'September':
+        month_int = 9
+    elif monthString == 'October':
+        month_int = 10
+    elif monthString == 'November':
+        month_int = 11
+    elif monthString == 'December':
+        month_int = 12
+    else:
+        month_int = 0
+
+    print(month_int)
+    return month_int
+
+
+user_string = input()
+
+get_month_as_int(user_string)
+get_month_as_int(new_date)
+
+# TODO: Read dates from input, parse the dates to find the one
+#       in the correct format, and output in m/d/yyyy format
