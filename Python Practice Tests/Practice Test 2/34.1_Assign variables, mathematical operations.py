1280832a8f78cbdfaadeea2e6d48f8547b161555
gregoryjstgermain <57375361+gregoryjstgermain@users.noreply.github.com>
2023-09-24 16:50:17 -0500

    Started Practice Test 2 Created 34.1 and 34.2

---
+++ Python Practice Tests/Practice Test 2/34.1_Assign variables, mathematical operations.py
@@ -0,0 +1,28 @@
+"""
+Task:
+
+Create a solution that accepts three integer inputs representing the number of times an employee travels to a job site.
+Output the total distance traveled to two decimal places given the following miles per employee commute to the job site.
+Output the total distance traveled to two decimal places given the following miles per employee commute to the job site:
+
+    Employee A: 15.62 miles
+    Employee B: 41.85 miles
+    Employee C: 32.67 miles
+
+The solution output should be in the format
+
+Distance: total_miles_traveled
+
+Sample Input/Output:
+
+If the input is
+
+1
+2
+3
+
+then the expected output is
+
+Distance: 197.33 miles
+
+"""
