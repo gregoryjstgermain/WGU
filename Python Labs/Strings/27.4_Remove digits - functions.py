ab38ee2cec8377b19c271698823926f76c561c1d
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-03 14:19:33 -0500

    Created and started 27.4 12.10 is still WIP

---
+++ Python Labs/Strings/27.4_Remove digits - functions.py
@@ -0,0 +1,27 @@
+"""
+Write a program that removes all digits from the given input.
+
+Ex: If the input is:
+
+1244Crescent
+
+the output is:
+
+Crescent
+
+The program must define and call the following function that takes a string as parameter and returns the
+string without any digits.
+
+def remove_digits(user_string)
+
+"""
+
+
+def remove_digits(user_string):
+
+
+# Type your code here.
+
+
+if __name__ == '__main__':
+# Type your code here.
