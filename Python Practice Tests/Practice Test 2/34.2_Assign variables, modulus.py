58866ca1c6f38a4bcef326741f03235948f91e16
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-08 10:45:54 -0600

    Working on 34.2

---
+++ Python Practice Tests/Practice Test 2/34.2_Assign variables, modulus.py
@@ -0,0 +1,29 @@
+"""
+Create a Python solution to the following task. Ensure that the solution produces output in exactly the same
+format shown in the sample(s) below, including capitalization and whitespace.
+Task:
+
+Create a solution that accepts an integer input representing any number of ounces. Output the converted total
+number of tons, pounds, and remaining ounces based on the input ounces value. There are 16 ounces in a pound
+and 2,000 pounds in a ton.
+
+The solution output should be in the format
+
+Tons: value_1
+Pounds: value_2
+Ounces: value_3
+
+Sample Input/Output:
+
+If the input is
+
+32035
+
+then the expected output is
+
+Tons: 1
+Pounds: 2
+Ounces: 3
+
+"""
+
