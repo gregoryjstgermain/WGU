913587241830ef3712698ca48db413c26cc39ff7
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-09 11:46:17 -0600

    Started 34.5

---
+++ Python Practice Tests/Practice Test 2/34.5_Data type conversions.py
@@ -0,0 +1,35 @@
+"""
+Create a solution that accepts five integer inputs. Output the sum of the five inputs three times, converting
+the inputs to the requested data type prior to finding the sum.
+
+    First output: sum of five inputs maintained as integer values
+    Second output: sum of five inputs converted to float values
+    Third output: sum of five inputs converted to string values (concatenate)
+
+The solution output should be in the format
+
+Integer: integer_sum_value
+Float: float_sum_value
+String: string_sum_value
+
+Sample Input/Output:
+
+If the input is
+
+1
+3
+6
+2
+7
+
+then the expected output is
+
+Integer: 19
+Float: 19.0
+"""
+
+#solution accepts five integer inputs
+#solution outputs three sums of input values; convert before calculating sum
+#first output: sum of five inputs maintained as integer values
+#second output: sum of five inputs converted to float values
+#third output: sum of five inputs converted to string values (concatenate)
