b44db99f2233a21c70f4ebbe84c6de8c19059119
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-09 16:47:28 -0600

    Finished 34.5 Started 34.6

--- Python Practice Tests/Practice Test 2/34.5_Data type conversions.py
+++ Python Practice Tests/Practice Test 2/34.5_Data type conversions.py
@@ -33,3 +33,17 @@ Float: 19.0
 #first output: sum of five inputs maintained as integer values
 #second output: sum of five inputs converted to float values
 #third output: sum of five inputs converted to string values (concatenate)
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
+
+int_total = num1 +num2 + num3 + num4 + num5
+float_total = float(num1 +num2 + num3 + num4 + num5)
+str_total = str(num1) + str(num2) + str(num3) + str(num4) + str(num5)
+
+print(f'Integer: {int_total}')
+print(f'Float: {float_total}')
+print(f'String: {str_total}')
---
+++ Python Practice Tests/Practice Test 2/34.6_Convert integer to formatted string.py
@@ -0,0 +1,27 @@
+"""
+Create a solution that accepts an integer input representing a 9-digit unformatted student identification
+number. Output the identification number as a string with no spaces.
+
+The solution output should be in the format
+
+111-22-3333
+
+Sample Input/Output:
+
+If the input is
+
+154175430
+
+then the expected output is
+
+154-17-5430
+
+"""
+
+#hint: modulo (%) and floored division (//) may be used
+#solution accepts a 9-digit integer representing an unformatted student identification number (i.e.,"5417543010")
+#solution outputs formatted student identification number as a string (i.e.,"541-75-3010")
+
+original_ID = int(input())
+formatted_ID = 0
+
