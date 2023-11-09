be7e13226fbcbb0d1b7351bc0d32f7bc1d1737f1
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-09 17:05:11 -0600

    Finished 34.8 Started 34.9

--- Python Practice Tests/Practice Test 2/34.8_Try block with exception error.py
+++ Python Practice Tests/Practice Test 2/34.8_Try block with exception error.py
@@ -35,3 +35,13 @@ frameworks = ["Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears"]
 #use try block with exception "Error" when index value is not found in list
 #solution accepts an integer input
 #solution outputs the corresponding string value for the integer input
+
+try:
+    index = int(input())
+    if index <= len(frameworks):
+        print(frameworks[index])
+    else:
+        raise KeyError
+
+except KeyError:
+    print('Error')
