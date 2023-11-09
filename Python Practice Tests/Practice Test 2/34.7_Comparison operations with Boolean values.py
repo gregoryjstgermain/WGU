d17a2f5d31df3de1cf65698f76a7344ace09c777
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-09 17:00:54 -0600

    Finished 34.7 Started 34.8

--- Python Practice Tests/Practice Test 2/34.7_Comparison operations with Boolean values.py
+++ Python Practice Tests/Practice Test 2/34.7_Comparison operations with Boolean values.py
@@ -26,3 +26,9 @@ predef_list = [4, -27, 15, 33, -10]
 #solution accepts an integer input
 #solution outputs Boolean value indicating if integer input is greater than the maximum value when compared to predef_list
 
+comp = int(input())
+
+if comp > max(predef_list):
+    print(f'Greater Than Max? {True}')
+elif comp < max(predef_list):
+    print(f'Greater Than Max? {False}')
