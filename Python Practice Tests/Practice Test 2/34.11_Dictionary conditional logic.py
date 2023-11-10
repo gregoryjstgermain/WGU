737d6bb49c0b497f219be5b68b97b1391bac9d97
gregoryjstgermain <gregoryjstgermain@gmail.com>
2023-11-10 15:25:07 -0600

    Update 34.11_Dictionary conditional logic.py

--- Python Practice Tests/Practice Test 2/34.11_Dictionary conditional logic.py
+++ Python Practice Tests/Practice Test 2/34.11_Dictionary conditional logic.py
@@ -38,3 +38,9 @@ then the expected output is
 cookies $585.79
 
 """
+purchase = {"bananas": 1.85, "steak": 19.99, "cookies": 4.52, "celery": 2.81, "milk": 4.34}
+
+#cost per item: <10 is full price, 10-20 (inclusive) is 5% discount, and 21+ is 10% discount
+#solution accepts a string input representing an item (dictionary key)
+#solution accepts an integer input representing the number of items to be purchased
+#solution outputs the item and total cost of purchase
